ec2c65100af90d8ae1c840b1254380b4aae5ca86
Seymur Kafkas <seymurkafkas@gmail.com>
2021-03-18 23:08:24 +0300

    Array- Matrix Transformation

--- AssignmentI.c
+++ AssignmentI.c
@@ -1,6 +1,11 @@
 #include <mpi.h>
 #include <stdio.h>
 
+int indexAt(int row, int column, Matrix *m)
+{
+    return m->dimension * row + column;
+}
+
 int main(int argc, char **argv)
 {
     // Initialize the MPI environment
