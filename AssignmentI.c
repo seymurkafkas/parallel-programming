88952ef176fae9b7571d40a900c04c8f2a033508
Seymur Kafkas <seymurkafkas@gmail.com>
2021-03-18 18:57:21 +0300

    Initialised Fox Algorithm Code

---
+++ AssignmentI.c
@@ -0,0 +1,11 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    // Initialize the MPI environment
+    MPI_Init(NULL, NULL);
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
+}
