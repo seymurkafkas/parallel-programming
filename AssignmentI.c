198e737937b28cf811122fdf51fd38974089068d
Seymur Kafkas <seymurkafkas@gmail.com>
2021-03-21 18:02:24 +0300

    Initialise the Grid and Communicators with MPI

--- AssignmentI.c
+++ AssignmentI.c
@@ -16,8 +16,8 @@ typedef struct
 typedef struct
 {
     MPI_Comm gridComm;
-    MPI_Comm rowCom;
-    MPI_Comm columnCom;
+    MPI_Comm rowComm;
+    MPI_Comm columnComm;
     int processCount;
     int gridOrder; //Assumed to be the square root processCount
     int myRank;
@@ -26,6 +26,48 @@ typedef struct
 } ProcessInfo;
 void readInputMatrix()
 {
+}
+
+void initialiseGrid(
+    ProcessInfo *grid /* out */)
+{
+    int prevRank;
+    int dimensions[2];
+    int isCircular[2];
+    int coordinates[2];
+    int freeCoords[2];
+
+    /* Set up Global Grid Information */
+    MPI_Comm_size(MPI_COMM_WORLD, &(grid->processCount));
+    MPI_Comm_rank(MPI_COMM_WORLD, &prevRank);
+
+    /* We assume p is a perfect square */
+    grid->gridOrder = (int)sqrt((double)grid->processCount);
+    dimensions[0] = dimensions[1] = grid->gridOrder;
+
+    /* We want a circular shift in second dimension. */
+    /* Don't care about first                        */
+    isCircular[0] = 1;
+    isCircular[1] = 1;
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dimensions,
+                    isCircular, 1, &(grid->gridComm));
+    MPI_Comm_rank(grid->gridComm, &(grid->myRank));
+    MPI_Cart_coords(grid->gridComm, grid->myRank, 2,
+                    coordinates);
+    grid->rowNumber = coordinates[0];
+    grid->columnNumber = coordinates[1];
+
+    /* Set up row communicators */
+    freeCoords[0] = 0;
+    freeCoords[1] = 1;
+    MPI_Cart_sub(grid->gridComm, freeCoords,
+                 &(grid->rowComm));
+
+    freeCoords[0] = 1;
+    freeCoords[1] = 0;
+
+    MPI_Cart_sub(grid->gridComm, freeCoords,
+                 &(grid->columnComm));
 }
 
 void multiplyMatrices(Matrix *first, Matrix *second)
